e00be968022599763a610b5127304ec7336b718c
Michiwoo <michiwoo.web@gmail.com>
2022-01-12 23:49:13 -0600

    :sparkles: Agregando vistas de login y registro. (#12)

---
+++ pages/auth/registro.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="w-3/12">
+    <form @submit.prevent="onSubmit">
+      <div class="mb-6">
+        <label for="user" class="block mb-2 text-base font-bold text-dark"
+          >Usuario</label
+        >
+        <input
+          v-model="userForm.usuario"
+          type="text"
+          class="
+            bg-gray-50
+            border border-gray-300
+            text-dark text-base
+            font-semibold
+            rounded-lg
+            focus:ring-black
+            focus:border-dark
+            block
+            w-full
+            p-2
+          "
+          placeholder=""
+          required
+        />
+      </div>
+      <div class="mb-6">
+        <label for="password" class="block mb-2 text-base font-bold text-dark"
+          >Contraseña</label
+        >
+        <input
+          v-model="userForm.password"
+          type="password"
+          class="
+            bg-gray-50
+            border border-gray-300
+            text-dark text-base
+            font-semibold
+            rounded-lg
+            focus:ring-black
+            focus:border-dark
+            block
+            w-full
+            p-2
+          "
+          placeholder=""
+          required
+        />
+      </div>
+      <button
+        type="submit"
+        class="
+          w-full
+          text-dark-text
+          bg-dark
+          hover:bg-black
+          focus:ring-4 focus:ring-dark
+          font-medium
+          rounded-lg
+          text-base
+          px-5
+          py-2
+          text-center
+          mr-2
+          mb-2
+        "
+      >
+        Iniciar Sesión
+      </button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userForm: {
+        usuario: null,
+        password: null,
+      },
+    }
+  },
+  methods: {
+    onSubmit() {
+      console.log(this.userForm)
+    },
+  },
+}
+</script>

9e4ec4b38eb3277cd67ec4b6b4b7559922e1afd1
Michiwoo <michiwoo.web@gmail.com>
2022-02-10 23:05:06 -0600

    :sparkles: Maquetado Emails (#55)

--- components/dashboard/Sidebar.vue
+++ components/dashboard/Sidebar.vue
@@ -72,7 +72,7 @@
           </div>
         </div>
       </router-link>
-      <router-link to="/dashboard">
+      <router-link to="/dashboard/emails">
         <div
           class="bg-light-text sm:px-2 flex justify-center items-center hover:text-blue-100 focus:text-blue-100 hover:bg-gray-500 focus:bg-gray-500 focus:outline-none focus:shadow-outline"
         >
@@ -82,7 +82,7 @@
             <div
               class="w-full sm:w-1/6 flex justify-center items-center m-4 sm:ml-4 sm:my-4 text-light-accent"
             >
-              Icon
+              <IconEmail class="text-light-accent w-4 h-4" />
             </div>
             <div
               class="hidden w-5/6 sm:flex justify-center text-base font-normal text-light-accent"
@@ -164,6 +164,7 @@ export default {
     IconForm: () => import('../icons/Formulario.vue'),
     IconWifi: () => import('../icons/Wifi.vue'),
     IconWorks: () => import('../icons/Wrench.vue'),
+    IconEmail: () => import('../icons/Envelope.vue'),
   },
 }
 </script>

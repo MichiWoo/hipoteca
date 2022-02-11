32487b3e831468e1172d178a93a24d1dbbcc588b
MichiWoo <michiwoo.web@gmail.com>
2022-02-10 22:11:35 -0600

    :sparkles: Maquetada sección de tareas.

---
+++ pages/dashboard/tareas.vue
@@ -0,0 +1,151 @@
+<template>
+  <div class="m-2 sm:m-0 w-full flex flex-col">
+    <div class="w-full flex flex-row justify-between items-center h-20">
+      <div class="flex flex-row justify-center items-center">
+        <div class="flex justify-center items-center mr-2">Lista de Tareas</div>
+      </div>
+      <div class="flex flex-row justify-center items-center">
+        <div class="flex items-center h-5 m-2">
+          <input
+            id="nocontactado"
+            v-model.number="filter"
+            type="radio"
+            class="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300"
+            value="0"
+          />
+          <div class="ml-3 text-sm">
+            <label
+              for="nocontactado"
+              class="font-medium text-gray-900 dark:text-gray-300"
+            >No Contactados</label>
+          </div>
+        </div>
+        <div class="flex items-center h-5 m-2">
+          <input
+            id="contactado"
+            v-model.number="filter"
+            type="radio"
+            class="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300"
+            value="1"
+          />
+          <div class="ml-3 text-sm">
+            <label for="contactado" class="font-medium text-gray-900 dark:text-gray-300">Contactado</label>
+          </div>
+        </div>
+        <div class="flex items-center h-5 m-2">
+          <input
+            id="esperando"
+            v-model.number="filter"
+            type="radio"
+            class="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300"
+            value="2"
+          />
+          <div class="ml-3 text-sm">
+            <label
+              for="esperando"
+              class="font-medium text-gray-900 dark:text-gray-300"
+            >Esperando Doc.</label>
+          </div>
+        </div>
+        <div class="flex items-center h-5 m-2">
+          <input
+            id="tramitando"
+            v-model.number="filter"
+            type="radio"
+            class="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300"
+            value="3"
+          />
+          <div class="ml-3 text-sm">
+            <label for="tramitando" class="font-medium text-gray-900 dark:text-gray-300">Tramitando</label>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+      <div class="inline-block py-2 min-w-full sm:px-6 lg:px-8">
+        <div class="overflow-hidden shadow-md sm:rounded-lg">
+          <TablaTareas :expedientes="expedientesShow" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  components: {
+    TablaTareas: () =>
+      import('../../components/dashboard/TablaExpedientes.vue'),
+  },
+  data() {
+    return {
+      expedientes: [],
+      expedientesShow: [],
+      errors: '',
+      filter: 0,
+    }
+  },
+  computed: {
+    ...mapGetters('expedientes', [
+        'expedientesNoContactados',
+        'expedientesContactados',
+        'expedientesEsperando',
+        'expedientesTramitando',
+        'expedientesFirmando',
+        'expedientesFallida',
+        'expedientesBanco'
+      ])
+  },
+  watch: {
+    filter() {
+      switch (this.filter) {
+        case 0:
+          this.expedientesShow = this.expedientesNoContactados
+          return
+        case 1:
+          console.log()
+          this.expedientesShow = this.expedientesContactados
+          return
+        case 2:
+          this.expedientesShow = this.expedientesEsperando
+          return
+        case 3:
+          this.expedientesShow = this.expedientesTramitando
+          return
+        case 4:
+          this.expedientesShow = this.expedientesFirmando
+          return
+        case 5:
+          this.expedientesShow = this.expedientesFallida
+          return
+        case 6:
+          this.expedientesShow = this.expedientesBanco
+          return
+        case 7:
+          this.expedientesShow = this.expedientes
+      }
+    }
+  },
+  mounted() {
+    this.getExpedientes()
+  },
+  methods: {
+    async getExpedientes() {
+      this.errors = ''
+      try {
+        const expedients = this.$store.state.expedientes.expedientes
+        if (expedients.length === 0) {
+          const resp = await this.$store.dispatch('expedientes/getExpedientes')
+          this.expedientes = resp
+        } else {
+          this.expedientes = expedients
+        }
+        this.expedientesShow = this.expedientes
+      } catch (error) {
+        console.log(error)
+      }
+    },
+  },
+}
+</script>

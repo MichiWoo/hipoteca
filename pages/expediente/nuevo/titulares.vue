9ee6fe0653b66034ddffd171b8fe4c6bc9cc847d
Michiwoo <michiwoo.web@gmail.com>
2022-02-28 23:36:53 -0600

    :sparkles: Agregada funcion parra agregar titulares. (#62)
    * :sparkles: Se maqueto formulario de titulares
    
    * :sparkles: Agregada funcion parra agregar titulares.

--- pages/expediente/nuevo/titulares.vue
+++ pages/expediente/nuevo/titulares.vue
@@ -4,7 +4,7 @@
       <button 
         type="button"
         class="text-light bg-light-primary hover:bg-light-danger focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 text-center mr-2 mb-2"
-        @click="verForm=true"
+        @click="verFormulario"
       >
         <div class="flex justify-center items-center">
           <IconPlus class="w-5 h-5" />
@@ -14,10 +14,14 @@
     </div>
     <div class="flex justify-start items-center">
       <ul>
-        <li v-for="(t, index) in titulares" :key="t.id">
-          <div class="flex justify-center items-center">
+        <li v-for="(t, index) in expediente.titulares" :key="t.id">
+          <div class="flex justify-start items-center">
             <span class="mr-2">Titular {{index + 1}} - {{t.nombre}}</span>
-            <button type="button" class="text-light bg-light-primary hover:bg-light-danger focus:ring-4 focus:ring-light-primary font-medium rounded-full text-sm p-2 text-center inline-flex items-center mr-2">
+            <button 
+              type="button"
+              class="text-light bg-light-primary hover:bg-light-danger focus:ring-4 focus:ring-light-primary font-medium rounded-full text-sm p-2 text-center inline-flex items-center mr-2"
+              @click="verTitular(t)"
+            >
               <svg class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
             </button>
           </div>
@@ -25,7 +29,19 @@
       </ul>
     </div>
     <div class="w-8/12 flex justify-start items-center my-6">
-      <FormTitulares v-show="verForm" :titular="titulares[0]" />
+      <FormTitulares v-show="verForm" @submitform="reloadTitular" @cancel="verForm = false" />
+    </div>
+    <div v-show="expediente.titulares.length > 1" class="flex justify-start items-center">
+      <button 
+        type="button"
+        class="text-light bg-light-primary hover:bg-light-danger focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 text-center mr-2 mb-2"
+        @click="goPrestamo"
+      >
+        <div class="flex justify-center items-center">
+          <span class="ml-2">Ir a Préstamos</span>
+        </div>
+      </button>
+
     </div>
   </div>
 </template>
@@ -39,12 +55,31 @@
     },
     data() {
       return {
-        titulares: [
-          { id: 1, nombre: "Michel González", edad: 37, dni: "x1a1x121x2", empleo: "Desarrollador", tipo: 1, antiguedad: 5, salario: 20000, pagos: 8000, renta: 0 },
-          { id: 2, nombre: "Yessica Vidal", edad: 40, dni: "rassa1222", empleo: "Ama de Casa", tipo: 5, antiguedad: 10, salario: 1000, pagos: 500, renta: 0 },
-        ],
         verForm: false,
+        titularSel: {},
       }
+    },
+    computed: {
+      expediente() {
+        return this.$store.state.expedientes.expedienteSel
+      },
+    },
+    methods: {
+      verFormulario() {
+        this.verForm = true
+      },
+      reloadTitular() {
+        this.verForm = false
+      },
+      verTitular(titular) {
+        console.log(titular)
+        this.$store.dispatch('expedientes/selectTitular', {})
+        this.$store.dispatch('expedientes/selectTitular', titular)
+        this.verForm = true
+      },
+      goPrestamo(){
+        this.$router.push('/expediente/nuevo/prestamo')
+      },
     },
   }
 </script>
